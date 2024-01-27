3b5f5d774054b92c65c3e09a55405c28a708c6e7
danielurralopez1234 <estylo7@gmail.com>
2024-01-27 02:02:29 -0300

    avance de migracion de template

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom'
+import NucleoIcons from './views/NucleoIcons.tsx'
+import LandingPage from './views/example/LandingPage.tsx'
+import ProfilePage from './views/example/ProfilePage.tsx'
+import LoginPage from './views/example/LoginPage.tsx'
+
+// styles for this kit
+import "./assets/css/bootstrap.min.css"
+import "./assets/scss/now-ui-kit.scss?v=1.5.0"
+import "./assets/demo/demo.css?v=1.5.0"
+import "./assets/demo/nucleo-icons-page-styles.css?v=1.5.0"
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
+// const root = ReactDOM.createRoot(document.getElementById("root")!);
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/index" element={<App />} />
+//       <Route path="/nucleo-icons" element={<NucleoIcons />} />
+//       <Route path="/landing-page" element={<LandingPage />} />
+//       <Route path="/profile-page" element={<ProfilePage />} />
+//       <Route path="/login-page" element={<LoginPage />} />
+
+//       <Route path="*" element={<Navigate to="/index" replace />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,5 +1,22 @@
 import { useEffect } from "react";
+
 import IndexHeader from "../components/Headers/IndexHeader";
+import IndexNavbar from "../components/Navbars/IndexNavbar";
+import Images from "./index-sections/Images";
+import BasicElements from "./index-sections/BasicElements";
+import Navbars from "./index-sections/Navbars";
+import Tabs from "./index-sections/Tabs";
+import PaginationSection from "./index-sections/Pagination";
+import Notifications from "./index-sections/Notifications";
+import Typography from "./index-sections/Typography";
+import Javascript from "./index-sections/Javascript";
+import CarouselSection from "./index-sections/Carousel";
+import NucleoIcons from "./index-sections/NucleoIcons";
+import CompleteExamples from "./index-sections/CompleteExamples";
+import SignUp from "./index-sections/SignUp";
+import Examples from "./index-sections/Examples";
+import Download from "./index-sections/Download";
+import DarkFooter from "../components/Footers/DarkFooter";
 
 
 function Index() {
@@ -16,7 +33,27 @@ function Index() {
   });
   return (
     <>
+      <IndexNavbar />
+      <div className="wrapper">
       <IndexHeader />
+      <div className="main">
+      <Images />
+          <BasicElements />
+          <Navbars />
+          <Tabs />
+          <PaginationSection />
+          <Notifications />
+          <Typography />
+          <Javascript />
+          <CarouselSection />
+          <NucleoIcons />
+          <CompleteExamples />
+          <SignUp />
+          <Examples />
+          <Download />
+      </div>
+      <DarkFooter />
+      </div>
     </>
   );
 }
