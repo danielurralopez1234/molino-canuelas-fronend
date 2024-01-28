aabbaffb1536dfee2c1d8f3c4d87baf34c0dd096
danielurralopez1234 <estylo7@gmail.com>
2024-01-28 11:58:37 -0300

    texto ini cial tipiado

--- src/components/Headers/IndexHeader.tsx
+++ src/components/Headers/IndexHeader.tsx
@@ -1,12 +1,11 @@
 import { createRef, useEffect } from "react";
 import { Container } from "reactstrap";
-import logoMolinoCanuelas from "./../../assets/img/logoMolinoCanuelas.png"
-import header from "./../../assets/img/header.jpg"
-import slim from "./../../assets/img/invision-white-slim.png"
-import slim2 from "./../../assets/img/creative-tim-white-slim2.png"
+import logoMolinoCanuelas from "./../../assets/img/logoMolinoCanuelas.png";
+import Typewriter from "typewriter-effect";
 
-
-
+import header from "./../../assets/img/header.jpg";
+import slim from "./../../assets/img/invision-white-slim.png";
+import slim2 from "./../../assets/img/creative-tim-white-slim2.png";
 
 const IndexHeader = () => {
   let pageHeader = createRef<any>();
@@ -31,45 +30,30 @@ const IndexHeader = () => {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + header + ")"
+            backgroundImage: "url(" + header + ")",
           }}
           ref={pageHeader}
         ></div>
         <Container>
           <div className="content-center brand">
-            <img
-              alt="..."
-              className="n-logo"
-              src={logoMolinoCanuelas}
-            ></img>
-            <h3>Somos el molino más grande de Sudamérica, nos enorgullece anunciar la presencia de nuestra prestigiosa marca de harina, Adelia María, en Chile, de la mano del distribuidor oficial Los Leños</h3>
+            <img alt="..." className="n-logo" src={logoMolinoCanuelas}></img>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+
+                  .pauseFor(100)
+                  .deleteAll()
+                  .typeString(
+                    "<h3>Somos el molino más grande de Sudamérica, nos enorgullece anunciar la presencia de nuestra prestigiosa marca de harina, Adelia María, en Chile, de la mano del distribuidor oficial Los Leños.</h3>"
+                  )
+                  .start();
+              }}
+            />
           </div>
-          {/* <h6 className="category category-absolute">
-            Designed by{" "}
-            <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
-              <img
-                alt="..."
-                className="invision-logo"
-                src={slim}
-              ></img>
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://www.creative-tim.com?ref=nukr-index-header"
-              target="_blank"
-            >
-              <img
-                alt="..."
-                className="creative-tim-logo"
-                src={slim2}
-              ></img>
-            </a>
-            .
-          </h6> */}
         </Container>
       </div>
     </>
   );
-}
+};
 
 export default IndexHeader;
