db898a2470cc5345ed69fe1443a3472bdef15747
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2024-01-29 11:30:21 -0300

    acpto cambios

--- src/views/index-sections/SignUp.tsx
+++ src/views/index-sections/SignUp.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import {
   Button,
   Card,
@@ -13,7 +12,6 @@ import {
   InputGroupText,
   InputGroup,
   Container,
-  
   Row
 } from "reactstrap";
 
